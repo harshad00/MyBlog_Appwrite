e8a0694e5506d0ea4f1d3909e814fe833b739686
Harshad 200 <bholo0090@gmail.com>
2024-06-02 09:47:38 +0530

    add css in Post.jsx

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -43,7 +43,7 @@ export default function Post() {
     };
 
     return post ? (
-        <div className="py-8 h-screen">
+        <div className="py-8 h-screen overflow-scroll">
             <Container>
                 <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
                     <img
