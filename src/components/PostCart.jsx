ff56a663869415874f5dc228de7768755eba901f
Harshad 200 <bholo0090@gmail.com>
2024-06-02 17:09:35 +0530

    Add athur name in Caet

--- src/components/PostCart.jsx
+++ src/components/PostCart.jsx
@@ -1,34 +1,20 @@
-// import React from 'react';
-// import appwriteService  from "../appwrit/confing"
-// import { Link } from 'react-router-dom';
 
-// function PostCart({$id, title, featureImage}) {
-//   return (
-//     <Link to={`/post/${$id}`}>
-//   <div className='w-full bf-gray-100 rounded-xl p-4'>
-//   <div className='w-full justify-center mb-4'>
-//     <img src={appwriteService.getFilePreview(featureImage)}
-//     alt="{title}" className=' rounded-lg' />
-
-//   </div>
-//   <h2 className='text-xl font-bold'>{title}</h2>
-//   </div>
-//     </Link>
-//   );
-// }
-
-// export default PostCart;
-
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import appwriteService from "../appwrit/confing";
+import appwriteService1 from "../appwrit/auth";
 
 function PostCart({ $id, title, featuredImage = "" }) {
+  const [user, setUser] = useState('');
   // Check if featureImage is provided and generate the preview URL
   const imageUrl = featuredImage
     ? appwriteService.getFilePreview(featuredImage)
     : "";
+
+    appwriteService1.getCurrentUser().then((currentUser) => {
+      setUser(currentUser);
+    });
 
   return (
     <Link to={`/post/${$id}`}>
@@ -39,7 +25,10 @@ function PostCart({ $id, title, featuredImage = "" }) {
             <img src={imageUrl} alt={title} className="rounded-lg" />
           </div>
         )}
-        <h2 className="text-xl font-bold">{title}</h2>
+        <div className="flex flex-col">
+        <h2 className="text-xl font-bold">{title}</h2> 
+        <p className=" text-sm px-1 font-semibold "> by {user.name} </p>
+        </div>
       </div> 
 
 
